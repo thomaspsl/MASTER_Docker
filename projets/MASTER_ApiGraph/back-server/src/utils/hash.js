a701dd908df1943c9cca086a77e9f91137c6bcbc
thomaspsl <thomas.peissel.tp@gmail.com>
2023-12-21 15:05:22 +0100

    feat: supp

--- projets/MASTER_ApiGraph/back-server/src/utils/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-module.exports = {
-  async hash(value) {
-    return await bcrypt.hash(value, 8);
-  },
-  async compare(first, second) {
-    return await bcrypt.compare(first, second);
-  },
-};
